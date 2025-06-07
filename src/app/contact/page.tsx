04f9de7f70d28754212207c8bac9cb25e1dc20ac
notansjwmember <73683836+notansjwmember@users.noreply.github.com>
2025-06-07 23:44:06 +0800

    refactor: add necessary dependencies

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,6 +6,20 @@ export default function Contact() {
   return (
     <section className="content h-screen flex flex-col">
       <h5 className="page-title">/ Contact</h5>
+      <form>
+        <div className="form-input">
+          <label htmlFor="name">Full Name</label>
+          <input type="text" />
+        </div>
+        <div className="form-input">
+          <label htmlFor="name">Email address</label>
+          <input type="email" />
+        </div>
+        <div className="form-input">
+          <label htmlFor="message">Message</label>
+          <textarea name="message"></textarea>
+        </div>
+      </form>
     </section>
   );
 }
